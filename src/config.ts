97eb06d0d56f52807ef05ae2ad5095580e481796
maxwang2 <maxwang2@163.com>
2025-10-31 21:39:06 +0800

    基础配置

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "VagerV",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	bio: "Student @ a high school.",
 	links: [
 		{
 			name: "Twitter",
